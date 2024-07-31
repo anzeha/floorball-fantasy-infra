ec9340df627766bfafb16fa2541b10a57dad2b26
Anze <anze603@gmail.com>
2024-07-31 14:19:37 +0200

    Cleanup

--- infrastructure-modules/vpc/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "vpc_network_name" {
-    value = module.vpc.network_name
-}
-output "vpc_subnetwork_name" {
-  value = module.vpc.subnets_names[0]
-}
--- infrastructure-modules/vpc/router.tf
+++
@@ -1,7 +0,0 @@
-resource "google_compute_router" "this" {
-  name       = format("%s-%s-cloud-router", var.cluster_name, var.env)
-  project    = var.project_id
-  region     = var.region
-  network    = module.vpc.network_name
-  depends_on = [module.vpc]
-}

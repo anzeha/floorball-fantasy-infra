9b552d5f39dab8623c07a1eec03dd636eca731b5
Anze <anze603@gmail.com>
2024-08-05 13:29:14 +0200

    Added nginx deployment

--- infrastructure-modules/kubernetes-addons/main.tf
+++ infrastructure-modules/kubernetes-addons/main.tf
@@ -5,40 +5,10 @@ resource "google_compute_address" "ingress" {
   region  = var.region
 }
 
-resource "kubernetes_namespace" "example" {
-  metadata {
-    annotations = {
-      name = "example-annotation"
-    }
+module "nginx-controller" {
+  count = var.deploy_nginx ? 1 : 0
+  source = "terraform-iaac/nginx-controller/helm"
 
-    labels = {
-      mylabel = "label-value"
-    }
+  ip_address = google_compute_address.ingress[0].address
 
-    name = "terraform-example-namespace"
-  }
 }
-
-
-resource "kubernetes_namespace" "example1" {
-  metadata {
-    annotations = {
-      name = "example-annotation1"
-    }
-
-    labels = {
-      mylabel = "label-value1"
-    }
-
-    name = "terraform-example-namespace1"
-  }
-}
-
-
-# module "nginx-controller" {
-#   count = var.deploy_nginx ? 1 : 0
-#   source = "terraform-iaac/nginx-controller/helm"
-
-#   ip_address = google_compute_address.ingress[0].address
-
-# }

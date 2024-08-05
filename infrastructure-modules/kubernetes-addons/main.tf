f792a706229ad43bcd5ff7e87113c2d576009ff7
Anze <anze603@gmail.com>
2024-08-05 08:58:23 +0200

    test

--- infrastructure-modules/kubernetes-addons/main.tf
+++ infrastructure-modules/kubernetes-addons/main.tf
@@ -5,6 +5,20 @@ resource "google_compute_address" "ingress" {
   region  = var.region
 }
 
+resource "kubernetes_namespace" "example" {
+  metadata {
+    annotations = {
+      name = "example-annotation"
+    }
+
+    labels = {
+      mylabel = "label-value"
+    }
+
+    name = "terraform-example-namespace"
+  }
+}
+
 
 module "nginx-controller" {
   count = var.deploy_nginx ? 1 : 0
